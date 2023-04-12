fd1eed9ec442b7c1723917ba82909394c48363a8
Jake Nicodemus <jakenicodemus@aol.com>
2023-04-11 19:09:10 -0500

    added database and auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { db } from "@/lib/db"
+
+export default async function Home() {
+  await db.set('hello', 'hello')
+
   return (
     <div>Hello World</div>
   )
