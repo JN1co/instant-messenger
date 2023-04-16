3a4943de47f49d44214a16589e62d905ddd40b45
Jake Nicodemus <jakenicodemus@aol.com>
2023-04-16 17:08:56 -0500

    add friend

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react"
 import Button from "@/components/ui/Button"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/auth"
 
-interface pageProps {}
+const page = async ({}) => {
 
-const page: FC<pageProps> = ({}) => {
-    return <Button>Hello</Button>
+    const session = await getServerSession(authOptions)
+
+    return <pre>{JSON.stringify(session)}</pre>
 }
 
 export default page
