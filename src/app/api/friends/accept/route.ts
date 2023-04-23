dd533e7d8af9621b52350c533c235f739b197437
Jake Nicodemus <jakenicodemus@aol.com>
2023-04-22 20:04:58 -0500

    accept/deny functionality

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,5 +1,6 @@
 import { fetchRedis } from "@/helpers/redis"
 import { authOptions } from "@/lib/auth"
+import { db } from "@/lib/db"
 import { getServerSession } from "next-auth"
 import { z } from "zod"
 
@@ -12,17 +13,36 @@ export async function POST(req: Request) {
         const session = await getServerSession(authOptions)
 
         if(!session) {
-            return new Response('Unauthorized', {status: 401})
+            return new Response('Unauthorized', { status: 401 })
         }
 
-        const isAlreadyFriends = await fetchRedis('sismember', `user:${session.user.id}:friends`)
+        const isAlreadyFriends = await fetchRedis('sismember', `user:${session.user.id}:friends`, idToAdd)
 
         if(isAlreadyFriends) {
-            return new Response('Already friends', {status: 400})
+            return new Response('Already friends', { status: 400 })
         }
 
         const hasFriendRequest = await fetchRedis('sismember', `user:${session.user.id}:incoming_friend_requests`, idToAdd)
+
+        if (!hasFriendRequest) {
+            return new Response('No friend request', { status: 400 })
+        }
+
+        await db.sadd(`user:${session.user.id}:friends`, idToAdd)
+
+        await db.sadd(`user:${idToAdd}:friend`, session.user.id)
+
+        await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd)
+
+        return new Response('OK')
+
     } catch (error) {
+        console.log(error)
         
+        if(error instanceof z.ZodError) {
+            return new Response('Invalid request payload', { status: 422 })
+        }
+
+        return new Response('Invalid request', { status: 400 })
     }
 }
--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -17,7 +17,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({incomingFriendRequests, sessio
     )
 
     const acceptFriend = async (senderId: string) => {
-        await axios.post('/api/friend/accept', {id: senderId})
+        await axios.post('/api/friends/accept', {id: senderId})
 
         setFriendRequests((prev) => 
             prev.filter((request) => request.senderId !== senderId)
@@ -27,7 +27,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({incomingFriendRequests, sessio
     }
 
     const denyFriend = async (senderId: string) => {
-        await axios.post('/api/friend/deny', {id: senderId})
+        await axios.post('/api/friends/deny', {id: senderId})
 
         setFriendRequests((prev) => 
             prev.filter((request) => request.senderId !== senderId)
